1e9a45bfeb76a381847a55107c2e1abd7d159051
VijarraM <121908120+VijarraM@users.noreply.github.com>
2024-05-28 19:19:59 -0300

    se deja solo front

--- api/src/routes/temperaments.routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const findAll = require('../controllers/temperaments.controllers');
-
-const router = Router();
-
-router.get('/', findAll);
-
-module.exports = router;
